c3ad82ab6717569010774c9d569492ce202096bb
Wjatscheslaw Michailov <mikhailowvw@gmail.com>
2025-05-04 14:22:34 +0300

    Give GPU to VLC user

--- sound.sh
+++ sound.sh
@@ -13,5 +13,6 @@ setfacl -m u:100021:rw /dev/video0
 # Give Wayland sayonara sound
 setfacl -R -m u:109823:rw /dev/snd
 
-# Give Wayland VLC sound
+# Give Wayland VLC sound 
 setfacl -R -m u:109813:rw /dev/snd
+setfacl -m u:109813:rw /dev/dri/card0

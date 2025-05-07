cd7d3cac960951dadc26a7f931133e85e7643c38
Wjatscheslaw Michailov <mikhailowvw@gmail.com>
2025-05-07 16:53:02 +0300

    rm redumdant Dockerfile

--- keepass/run_wlnd.sh
+++ keepass/run_wlnd.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-IMAGE=$1
-COMMAND=$2
-x11docker --clipboard=yes --network=none --user=RETAIN --backend=podman -- --uidmap=0:119:1 --uidmap=9898:120:1 --gidmap=0:119:1 --gidmap=9898:120:1  -v /opt/keepass:/home/testpilot:rw -- $IMAGE $COMMAND
+make run-wayland
